2a55bb6f4ca1f4acac95fa498a666e3204c8e2bc
ubwoo4175 <59498073+ubwoo4175@users.noreply.github.com>
2020-01-11 22:37:33 +0900

    Add files via upload

---
+++ 2. R Programming/ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,32 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x
+	setinv <- function(i) inv <<- i
+	getinv <- function() inv
+	list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+	inv <- x$getinv()
+	if(!is.null(inv)) {
+		message('getting cached data')
+		return(inv)
+	}
+	matrix <- x$get()
+	inv <- solve(matrix)
+	x$setinv(inv)
+	inv
+}
